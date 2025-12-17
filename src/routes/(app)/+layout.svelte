41eff707e95f4052bf59d253a17244416fb167ac
0xBADC0FFEE <a.burtsev@seed.me>
2025-12-17 15:39:16 +0300

    feat: add authorization page content

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -1,9 +1,27 @@
 <script lang="ts">
+  import { Screen } from '$lib/components/shell'
   import { Authenticated } from '@/iam/ui'
+  import Languages from '@/iam/ui/Languages.svelte'
+  import Footer from './Footer.svelte'
+  import Logo from './logo.svg'
 
   const { children } = $props()
 </script>
 
 <Authenticated>
+  {#snippet screen({ authentication })}
+    <Screen>
+      <div class="flex-1 flex flex-col justify-between">
+        <Languages />
+        <div class="flex-1 flex flex-col items-center px-4 max-w-sm mx-auto">
+          <div class="py-[10vh]">
+            <img src={Logo} alt="seed.me" class="h-10 mx-auto" />
+          </div>
+          {@render authentication()}
+        </div>
+        <Footer />
+      </div>
+    </Screen>
+  {/snippet}
   {@render children()}
 </Authenticated>
---
+++ src/routes/(app)/Footer.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import SeedMe from './seedme.svg'
+</script>
+
+<footer class="flex flex-col gap-3 p-4">
+  <div class="flex justify-center gap-2">
+    Made by
+    <img src={SeedMe} alt="seed.me" class="h-6" />
+  </div>
+  <div class="flex justify-center gap-2">
+    <a href="/terms/">Terms & Conditions</a>
+    &bull;
+    <a href="/privacy/">Privacy Policy</a>
+  </div>
+</footer>
