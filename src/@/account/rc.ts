7ffa02c5b553da470316dd29491a4065de59e94f
0xBADC0FFEE <a.burtsev@seed.me>
2025-12-19 19:13:30 +0300

    fix(account): remove duplicate subscription on start

--- src/@/account/rc.ts
+++ src/@/account/rc.ts
@@ -12,17 +12,15 @@ function subscriber(account: Account | null) {
   defaults(account)
 }
 
-account.subscribe((account) => {
-  if (account === null && unsubscribe === null)
-    subscribe()
-})
-
 function subscribe() {
   unsubscribe = account.subscribe(subscriber)
 }
 
 function rc() {
-  subscribe()
+  account.subscribe((account) => {
+    if (account === null && unsubscribe === null)
+      subscribe()
+  })
 }
 
 export { rc }
