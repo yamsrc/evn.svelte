94495b4aab30145e58346bbf5e53f73f5d26d2ac
0xBADC0FFEE <a.burtsev@seed.me>
2025-12-24 18:24:38 +0300

    feat(contacts): add share button component

---
+++ src/lib/components/share/Share.svelte
@@ -0,0 +1,54 @@
+<script lang="ts">
+  import { Share2 } from '@lucide/svelte'
+  import { browser } from '$app/environment'
+  import { Loader } from '$com/loader'
+  import { cn } from '$lib/utils'
+  import { Button } from '$ui/button'
+  import type { Props, Retriever } from './Share'
+
+  const {
+    data,
+    label = 'Share',
+    variant = 'outline',
+    disabled,
+    onshare,
+    class: classes,
+    ...rest
+  }: Props = $props()
+
+  let waiting = $state(false)
+
+  const unavailable = browser && !navigator.share
+
+  async function onclick() {
+    const share = typeof data === 'function' ? await get(data) : data
+
+    await navigator.share(share)
+    onshare?.()
+  }
+
+  async function get(fn: Retriever) {
+    waiting = true
+
+    const value = await fn()
+
+    waiting = false
+
+    return value
+  }
+</script>
+
+<Button
+  class={cn(classes)}
+  {variant}
+  {onclick}
+  disabled={unavailable || waiting || disabled}
+  {...rest}
+>
+  {#if waiting}
+    <Loader />
+  {:else}
+    <Share2 />
+  {/if}
+  {#if label !== null}{label}{/if}
+</Button>
--- src/routes/(private)/contacts/+page.svelte
+++ src/routes/(private)/contacts/+page.svelte
@@ -1,10 +1,22 @@
 <script lang="ts">
-  import { ArrowUpDown, QrCode, Share2 } from '@lucide/svelte'
+  import { ArrowUpDown, QrCode } from '@lucide/svelte'
   import { Async } from 'svas'
+  import { ok } from 'svas'
   import { Section } from '$com/section'
+  import { Share } from '$com/share'
+  import { origin } from '$config'
   import { dict } from '$lib/intl'
   import { Button } from '$ui/button'
   import { contacts } from '@/contacts'
+  import { account } from '@/iam'
+
+  const invitation = $derived(
+    ok($account)
+      ? {
+          url: `${origin}/join/${$account.id}/`,
+        }
+      : undefined,
+  )
 </script>
 
 <Section class="flex flex-col gap-6 pt-2">
@@ -22,14 +34,17 @@
 </Section>
 <Async store={contacts} class="flex-1 flex flex-col">
   {#snippet awaited(contacts)}
-    {#if contacts.length === 0}
+    {#if contacts.length === 0 && invitation}
       <Section class="flex-1 flex flex-col items-center justify-center space-y-2">
         <h2>{$dict.contacts.empty.title}</h2>
         <p>{$dict.contacts.empty.description}</p>
-        <Button variant="secondary" size="lg" class="w-full">
-          <Share2 />
-          {$dict.contacts.empty.invite.share}
-        </Button>
+        <Share
+          variant="secondary"
+          size="lg"
+          class="w-full"
+          data={invitation}
+          label={$dict.contacts.empty.invite.share}
+        />
         <Button variant="secondary" size="lg" class="w-full">
           <QrCode />
           {$dict.contacts.empty.invite.qr}
