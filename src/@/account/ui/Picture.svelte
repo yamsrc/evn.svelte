6ecd23e3064b0b356f4cedd4184a425612fd7fc8
0xBADC0FFEE <a.burtsev@seed.me>
2025-12-25 11:02:31 +0300

    fix(account): fix view transition names

--- src/@/account/ui/Picture.svelte
+++ src/@/account/ui/Picture.svelte
@@ -14,6 +14,6 @@
     width={size}
     height={size}
     class={cn('rounded-full', classes)}
-    style="view-transition-name: account-picture"
+    style={`view-transition-name: account-picture-${account.id};`}
   />
 {/if}
