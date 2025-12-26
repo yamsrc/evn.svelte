ea809476276459652b0a77b98d4eb8b8a919931b
0xBADC0FFEE <a.burtsev@seed.me>
2025-12-26 15:52:16 +0300

    feat(groups): make groups list collapsible

--- src/@/groups/ui/Groups.svelte
+++ src/@/groups/ui/Groups.svelte
@@ -1,9 +1,15 @@
 <script lang="ts">
+  import { ChevronUp, ChevronDown } from '@lucide/svelte'
   import Section from '$com/section/Section.svelte'
+  import { cn } from '$lib/utils'
+  import { buttonVariants } from '$ui/button'
+  import * as Collapsible from '$ui/collapsible'
   import Group from './Group.svelte'
   import type { Props } from './Groups'
 
   let { groups, title, selection = $bindable() }: Props = $props()
+
+  let open = $state(true)
 
   const selectable = $derived(!!selection)
 
@@ -15,13 +21,28 @@
   }
 </script>
 
-<Section class="flex flex-col gap-1.5">
-  {#if title}
-    <h2>{title}</h2>
-  {/if}
-  {#each groups as group (group.id)}
-    {@const selected = selection?.has(group.id)}
-    {@const selectedProps = selectable ? { selected, onselect } : undefined}
-    <Group {group} {...selectedProps} />
-  {/each}
+<Section>
+  <Collapsible.Root bind:open class="flex flex-col gap-1.5">
+    {#if title}
+      <div class="flex items-center justify-between">
+        <h2>{title}</h2>
+        <Collapsible.Trigger
+          class={cn(buttonVariants({ variant: 'secondary', size: 'icon' }), 'rounded-full size-6')}
+        >
+          {#if open}
+            <ChevronUp class="size-4" />
+          {:else}
+            <ChevronDown class="size-4" />
+          {/if}
+        </Collapsible.Trigger>
+      </div>
+    {/if}
+    <Collapsible.Content class="flex flex-col gap-1.5">
+      {#each groups as group (group.id)}
+        {@const selected = selection?.has(group.id)}
+        {@const selectedProps = selectable ? { selected, onselect } : undefined}
+        <Group {group} {...selectedProps} />
+      {/each}
+    </Collapsible.Content>
+  </Collapsible.Root>
 </Section>
