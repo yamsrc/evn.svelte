309c34fb30791b580b15aab525b75af400c7c6be
0xBADC0FFEE <a.burtsev@seed.me>
2025-12-29 14:34:56 +0300

    refactor: fix code issues

--- src/@/contacts/ui/Invite.svelte
+++ src/@/contacts/ui/Invite.svelte
@@ -24,7 +24,8 @@
   />
   <QR variant="secondary" {...rest} data={invitation.url} label={$dict.contacts.empty.invite.qr} />
   <Separator class="p-2 font-bold">{$dict.etc.or}</Separator>
-  <Button href="/contacts/new/manual/" {...rest}
-    ><UserPlus2 />{$dict.contacts.empty.invite.manual}</Button
-  >
+  <Button href="/contacts/new/manual/" {...rest}>
+    <UserPlus2 />
+    {$dict.contacts.empty.invite.manual}
+  </Button>
 </div>
