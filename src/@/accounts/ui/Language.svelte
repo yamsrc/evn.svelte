9fc3cc1b9c1eeddc42db8792da5bdbb9d1bfd6af
temich <tema.gurtovoy@gmail.com>
2025-12-30 22:33:44 +0400

    feat(accounts): hide `ru-FU`

--- src/@/accounts/ui/Language.svelte
+++ src/@/accounts/ui/Language.svelte
@@ -1,20 +1,40 @@
 <script lang="ts">
+  import { onMount } from 'svelte'
   import { locale } from '$lib/intl'
   import { Select, SelectTrigger, SelectContent, SelectItem } from '$ui/select'
   import { set } from '@/iam'
-  import { options } from './Language'
+  import { options, hidden } from './Language'
 
   function change(locale: string) {
     set({ locale })
   }
+
+  let opens = $state(0)
+  let revealed = $state(false)
+
+  function onOpenChange(open: boolean) {
+    if (!open) return
+
+    if (++opens === 5) revealed = true
+  }
+
+  onMount(() => (revealed = hidden.some((option) => option.value === $locale)))
 </script>
 
-<Select type="single" value={$locale} onValueChange={change}>
-  {@const selected = options.find((option) => option.value === $locale)}
+<Select type="single" value={$locale} onValueChange={change} {onOpenChange}>
+  {@const selected =
+    options.find((option) => option.value === $locale) ??
+    hidden.find((option) => option.value === $locale)}
   <SelectTrigger class="w-full">{selected?.label}</SelectTrigger>
   <SelectContent>
     {#each options as option (option.value)}
       <SelectItem value={option.value}>{option.label}</SelectItem>
     {/each}
+
+    {#if revealed}
+      {#each hidden as option (option.value)}
+        <SelectItem value={option.value}>{option.label}</SelectItem>
+      {/each}
+    {/if}
   </SelectContent>
 </Select>
