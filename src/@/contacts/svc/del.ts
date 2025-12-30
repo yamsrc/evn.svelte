b111a3fc1e984e0e71e5590aa1035558b2443274
temich <tema.gurtovoy@gmail.com>
2025-12-30 09:34:10 +0400

    fead(contacts): add managed account creation

--- src/@/contacts/svc/del.ts
+++ src/@/contacts/svc/del.ts
@@ -1,12 +1,11 @@
-import { having } from 'svas'
+import { ensure } from 'svas'
 import { account } from '@/iam'
 import * as net from './net'
 import { internal } from './store'
 import type { Contact } from './Contact'
 
 export async function del(contact: Contact): Promise<void | Error> {
-  const me = await having(account)
-
+  const me = ensure(account)
   const res = await net.del(me.id, contact.identity)
 
   if (res instanceof Error) return res
--- src/@/contacts/ui/Create.svelte
+++ src/@/contacts/ui/Create.svelte
@@ -1,8 +1,14 @@
 <script lang="ts">
+  import { goto } from '$app/navigation'
   import { Cosmetics, type Value } from '@/app/ui'
+  import { add } from '@/contacts'
 
   async function onchange(value: Value) {
-    console.log('create', value)
+    const contact = await add({ name: value.name, picture: value.picture })
+
+    if (contact instanceof Error) return
+
+    await goto(`/contacts/${contact.id}/`)
   }
 </script>
 
