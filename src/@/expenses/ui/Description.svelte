c6e87fbc055a744ba086e4d5043b41c5f0c1685b
0xBADC0FFEE <a.burtsev@seed.me>
2026-01-06 11:44:46 +0300

    refactor(expenses): extract description

---
+++ src/@/expenses/ui/Description.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  import { dict } from '$lib/intl'
+  import { Input } from '$ui/input'
+  import { Section } from '@/app/ui'
+  import type { Props } from './Description'
+
+  let { title = $bindable(), location = $bindable() }: Props = $props()
+</script>
+
+<Section class="space-y-5">
+  <fieldset class="space-y-2">
+    <Input
+      bind:value={title}
+      name="title"
+      required
+      placeholder={$dict.expenses.form.title.placeholder}
+      class="text-3xl font-bold"
+    />
+    <Input
+      bind:value={location}
+      name="location"
+      placeholder={$dict.expenses.form.location.placeholder}
+    />
+  </fieldset>
+</Section>
--- src/@/expenses/ui/Form.svelte
+++ src/@/expenses/ui/Form.svelte
@@ -3,9 +3,9 @@
   import { dict } from '$lib/intl'
   import { onsubmit } from '$lib/tools'
   import { Button } from '$ui/button'
-  import { Input } from '$ui/input'
   import { Section } from '@/app/ui'
   import { add, update } from '@/expenses'
+  import Description from './Description.svelte'
   import Payers from './Payers.svelte'
   import Spendings from './Spendings.svelte'
   import type { Props } from './Form'
@@ -31,22 +31,7 @@
 </script>
 
 <form onsubmit={onsubmit(submit)} class="space-y-5">
-  <Section class="space-y-5">
-    <fieldset class="space-y-2">
-      <Input
-        bind:value={expense.title}
-        name="title"
-        required
-        placeholder={$dict.expenses.form.title.placeholder}
-        class="text-3xl font-bold"
-      />
-      <Input
-        bind:value={expense.location}
-        name="location"
-        placeholder={$dict.expenses.form.location.placeholder}
-      />
-    </fieldset>
-  </Section>
+  <Description bind:title={expense.title} bind:location={expense.location} />
 
   <Separator />
 
