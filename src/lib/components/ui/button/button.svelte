d5e0a30bee3bbcd52f4b20118125f3f1054ab0ec
0xBADC0FFEE <a.burtsev@seed.me>
2026-01-09 18:29:51 +0300

    refactor(button): remove unnecessary class

--- src/lib/components/ui/button/button.svelte
+++ src/lib/components/ui/button/button.svelte
@@ -7,8 +7,7 @@
     base: "focus-visible:border-ring focus-visible:ring-ring/50 aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive inline-flex shrink-0 items-center justify-center gap-2 rounded-lg text-sm font-medium whitespace-nowrap transition-all outline-none focus-visible:ring-[3px] disabled:pointer-events-none disabled:brightness-50 aria-disabled:pointer-events-none aria-disabled:brightness-50 [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4",
     variants: {
       variant: {
-        default:
-          'bg-primary text-primary-foreground hover:brightness-90 shadow-xs disabled:brightness-50',
+        default: 'bg-primary text-primary-foreground hover:brightness-90 shadow-xs',
         destructive:
           'bg-destructive hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60 text-white shadow-xs',
         outline:
