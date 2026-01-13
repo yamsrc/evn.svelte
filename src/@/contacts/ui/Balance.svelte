3990ce0b50f1865dcbb774cad1d5cf14d5b1b9a6
temich <tema.gurtovoy@gmail.com>
2026-01-13 09:56:05 +0400

    feat(contacts): add `Balance`

---
+++ src/@/contacts/ui/Balance.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  import { dict } from '$lib/intl'
+  import { cn } from '$lib/utils'
+  import { Coins } from '@/app/ui'
+  import type { Props } from './Balance'
+
+  const { contact, class: classes }: Props = $props()
+</script>
+
+<div class={cn('flex gap-2', classes)}>
+  <div class="text-muted-foreground text-sm text-nowrap">
+    {#if contact.balance > 0}
+      {$dict.contacts.contact.owesYou}
+    {:else if contact.balance < 0}
+      {$dict.contacts.contact.youOwe}
+    {/if}
+  </div>
+  <Coins amount={contact.balance} />
+</div>
--- src/routes/(private)/contacts/[id=id]/+page.svelte
+++ src/routes/(private)/contacts/[id=id]/+page.svelte
@@ -2,13 +2,14 @@
   import { Async, ok } from 'svas'
   import { page } from '$app/state'
   import { Back } from '$com/history'
+  import { Separator } from '$com/separator'
   import { dict } from '$lib/intl'
   import { Spinner } from '$ui/spinner'
   import { Cosmetics } from '@/accounts/ui'
   import { Section } from '@/app/ui'
   import { Header } from '@/app/ui'
   import { contacts } from '@/contacts'
-  import { Share } from '@/contacts/ui'
+  import { Balance, Share } from '@/contacts/ui'
 
   const id = $derived(page.params.id) as string
 </script>
@@ -19,20 +20,26 @@
   </Header.Root>
 </Section>
 
-<Section>
-  <Async store={contacts}>
-    {#snippet awaited(contacts)}
-      {@const contact = contacts.find((contact) => contact.id === id)}
-      {#if contact?.account && ok(contact.account)}
+<Async store={contacts}>
+  {#snippet awaited(contacts)}
+    {@const contact = contacts.find((contact) => contact.id === id)}
+    {#if contact?.account && ok(contact.account)}
+      <Section>
         <div class="flex flex-col gap-4">
           <Cosmetics account={contact.account} editable={contact.managed} managed />
           {#if contact.managed}
             <Share {contact} />
           {/if}
         </div>
-      {:else}
-        <Spinner class="m-auto" />
-      {/if}
-    {/snippet}
-  </Async>
-</Section>
+      </Section>
+
+      <Separator />
+
+      <Section>
+        <Balance {contact} class="justify-center" />
+      </Section>
+    {:else}
+      <Spinner class="m-auto" />
+    {/if}
+  {/snippet}
+</Async>
