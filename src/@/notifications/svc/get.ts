275c3cfa9176c33a9ec0811881d81a7d696beed6
0xBADC0FFEE <a.burtsev@seed.me>
2026-01-22 22:46:33 +0300

    fix(notifications): remove dev code

--- src/@/notifications/svc/get.ts
+++ src/@/notifications/svc/get.ts
@@ -6,19 +6,5 @@ import type { Notification } from './net'
 export async function get(): Promise<Notification[] | Error> {
   const me = ensure(account)
 
-  const result = await net.get(me.id)
-
-  if (result instanceof Error) return result
-
-  const additional: Notification[] = Array.from({ length: 3 }, (_, i) => ({
-    id: `test-${i}`,
-    identity: 'test',
-    domain: 'accounts',
-    event: 'created',
-    key: `test-${i}`,
-    _created: new Date().getTime(),
-    _version: 1,
-  }))
-
-  return result.concat(additional)
+  return await net.get(me.id)
 }
