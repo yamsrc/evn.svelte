1c5fc91a4af4678611b6397a2054e67a249de465
temich <tema.gurtovoy@gmail.com>
2026-01-23 12:38:39 +0400

    feat(accounts): replace gender icon

--- src/@/accounts/ui/Grammar.ts
+++ src/@/accounts/ui/Grammar.ts
@@ -1,4 +1,4 @@
-import { Mars, Transgender, Venus, type Icon } from '@lucide/svelte'
+import { CircleSmall, Mars, Venus, type Icon } from '@lucide/svelte'
 import type { Account, Grammar } from '@/accounts'
 
 type AccountLike = Pick<Account, 'id' | 'name' | 'grammar'>
@@ -17,5 +17,5 @@ interface Option {
 export const options: Option[] = [
   { value: 'he', Icon: Mars },
   { value: 'she', Icon: Venus },
-  { value: 'they', Icon: Transgender },
+  { value: 'they', Icon: CircleSmall },
 ]
