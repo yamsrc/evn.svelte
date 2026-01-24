cb262e1af37a865002fa1c797ad59e50f28d0d53
temich <tema.gurtovoy@gmail.com>
2026-01-24 11:26:22 +0400

    feat(expenses): add search by participant

--- src/@/expenses/svc/filter.ts
+++ src/@/expenses/svc/filter.ts
@@ -1,10 +1,12 @@
 import { search } from '$lib/tools'
-import type { Expense } from './net'
+import type { Expense } from './store'
 
 export function filter(expenses: Expense[], query?: string): Expense[] {
   return search(expenses, query, stringify)
 }
 
 function stringify(expense: Expense): string {
-  return expense.title + ' ' + expense.location
+  const names = Object.values(expense.accounts).map((account) => account?.name).filter((name) => name !== undefined).join(' ')
+
+  return [expense.title, expense.location, names].join(' ')
 }
