b57087bdb1c5d2002946953f431fa2211140296f
temich <tema.gurtovoy@gmail.com>
2026-01-24 12:39:56 +0400

    fix(expenses): remove obsolete code

--- src/@/expenses/ui/Editor/Form/Form.ts
+++ src/@/expenses/ui/Editor/Form/Form.ts
@@ -21,7 +21,6 @@ export function normalize(value: Value, mode: 'sums' | 'shares'): Value {
   return {
     title: value.title,
     location: value.location,
-    date: value.date,
     participants,
     extras: value.extras.filter((extra) => extra.amount !== 0),
     attachments: value.attachments,
