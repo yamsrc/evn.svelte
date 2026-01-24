dbf73bba7779980df6774c98314249efcb1ebb40
temich <tema.gurtovoy@gmail.com>
2026-01-24 11:04:14 +0400

    refactor(expenses): extract stringify

--- src/@/expenses/svc/filter.ts
+++ src/@/expenses/svc/filter.ts
@@ -2,5 +2,9 @@ import { search } from '$lib/tools'
 import type { Expense } from './net'
 
 export function filter(expenses: Expense[], query?: string): Expense[] {
-  return search(expenses, query, (expense) => expense.title + ' ' + expense.location)
+  return search(expenses, query, stringify)
+}
+
+function stringify(expense: Expense): string {
+  return expense.title + ' ' + expense.location
 }
