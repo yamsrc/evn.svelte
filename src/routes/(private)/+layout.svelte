db5c0fac92321f1777abc47a3f31cc596b3169de
0xBADC0FFEE <a.burtsev@seed.me>
2026-01-26 16:44:07 +0300

    refactor(nav): make nav independent of notifications load

--- src/routes/(private)/+layout.svelte
+++ src/routes/(private)/+layout.svelte
@@ -1,23 +1,21 @@
 <script lang="ts">
-  import { Async } from 'svas'
+  import { ok } from 'svas'
+  import { derived } from 'svelte/store'
   import { Nav } from '$com/shell'
   import { oidc } from '$config'
   import { dict } from '$lib/intl'
   import { Screen, Authenticated } from '@/app/ui'
-  import { notifications } from '@/notifications'
+  import { notifications as store } from '@/notifications'
   import { sections } from './sections'
   import { welcome } from './welcome'
 
   const { children } = $props()
+  const notifications = derived(store, ($n) => (ok($n) ? $n : []))
 </script>
 
 <Screen>
   <Authenticated {oidc} oncreate={welcome}>
     {@render children()}
-    <Async store={notifications}>
-      {#snippet awaited(notifications)}
-        <Nav position="start" sections={sections($dict, notifications)} class="z-1000" />
-      {/snippet}
-    </Async>
+    <Nav position="start" sections={sections($dict, $notifications)} class="z-1000" />
   </Authenticated>
 </Screen>
