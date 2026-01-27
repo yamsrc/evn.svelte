6656f39d7b4ec8386d8432c452ccd9f8db6e9ef8
0xBADC0FFEE <a.burtsev@seed.me>
2026-01-27 11:38:33 +0300

    fix(transfer): return backgorund color

--- src/@/expenses/ui/views/Transfer.svelte
+++ src/@/expenses/ui/views/Transfer.svelte
@@ -36,7 +36,7 @@
   <Async store={combined(accounts.get(identity), contacts)}>
     {#snippet awaited([account, contacts])}
       {@const contact = contacts.find((c) => c.identity === identity)}
-      <Panel href={`/contacts/${contact?.id}/`}>
+      <Panel href={`/contacts/${contact?.id}/`} class="bg-constructive/20 border-constructive/30">
         {#snippet left()}
           <div class="flex items-center gap-3">
             <Avatar {account} />
