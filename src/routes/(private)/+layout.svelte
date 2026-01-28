b886fc65855cd372567aa6691fb11214d33895c7
temich <tema.gurtovoy@gmail.com>
2026-01-28 23:01:35 +0400

    fix: fix layout

--- src/routes/(private)/+layout.svelte
+++ src/routes/(private)/+layout.svelte
@@ -15,7 +15,7 @@
 
 <Screen>
   <Authenticated {oidc} oncreate={welcome}>
-    <div class="w-full max-w-2xl mx-auto space-y-5">
+    <div class="w-full max-w-2xl mx-auto space-y-5 m-auto">
       {@render children()}
       <Nav position="start" sections={sections($dict, $notifications)} class="z-1000" />
     </div>
