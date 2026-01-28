4bc4400c0c7a7b49d5dd17212ba1e8cb03ec81b9
temich <tema.gurtovoy@gmail.com>
2026-01-28 23:39:10 +0400

    feat: add error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,12 @@
+<script>
+  import { dict } from '$lib/intl/err'
+  import { Button } from '$ui/button'
+</script>
+
+<div class="min-h-screen flex flex-col items-center justify-center gap-4 p-8">
+  <section class="text-center">
+    <p>{$dict.message}</p>
+    <p class="text-muted-foreground">{$dict.background}</p>
+  </section>
+  <Button href="/">{$dict.action}</Button>
+</div>
