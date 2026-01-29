eaaab43c24055e03b10e232208eddcab4c6ec962
temich <tema.gurtovoy@gmail.com>
2026-01-30 01:29:10 +0400

    feat: add service worker

---
+++ src/service-worker.ts
@@ -0,0 +1,72 @@
+/// <reference no-default-lib="true"/>
+/// <reference lib="esnext" />
+/// <reference lib="webworker" />
+/// <reference types="@sveltejs/kit" />
+
+import { build, version } from '$service-worker'
+
+const app = globalThis.self as unknown as ServiceWorkerGlobalScope
+
+const CACHE = `cache-${version}`
+const ASSETS = [...build]
+
+app.addEventListener('install', (event) => {
+  async function install() {
+    const cache = await caches.open(CACHE)
+
+    await cache.addAll(ASSETS)
+
+    console.log('Assets cached', ASSETS.length)
+    console.log('App installed', version)
+  }
+
+  event.waitUntil(install())
+})
+
+app.addEventListener('activate', (event) => {
+  async function deleteOldCaches() {
+    for (const key of await caches.keys())
+      if (key !== CACHE) await caches.delete(key)
+  }
+
+  event.waitUntil(deleteOldCaches())
+
+  console.log('App activated', version)
+})
+
+app.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return
+
+  async function respond() {
+    const url = new URL(event.request.url)
+    const cache = await caches.open(CACHE)
+
+    if (ASSETS.includes(url.pathname)) {
+      const response = await cache.match(url.pathname)
+
+      if (response !== undefined)
+        return response
+    }
+
+    try {
+      const response = await fetch(event.request)
+
+      if (!(response instanceof Response))
+        throw new Error('invalid response from fetch')
+
+      if (response.status === 200)
+        cache.put(event.request, response.clone())
+
+      return response
+    } catch (err) {
+      const response = await cache.match(event.request)
+
+      if (response !== undefined)
+        return response
+
+      throw err
+    }
+  }
+
+  event.respondWith(respond())
+})
