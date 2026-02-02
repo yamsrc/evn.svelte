b2eb5a10ca082afc2592a6443827d5bdbd54007a
temich <tema.gurtovoy@gmail.com>
2026-02-03 01:22:51 +0400

    fix(transmission): fix intl imports

--- src/@/transmission/ui/intl/index.ts
+++ src/@/transmission/ui/intl/index.ts
@@ -1,9 +1,9 @@
 import { derived } from 'svelte/store'
 import { locale } from '$lib/intl'
-import { dictionaries, locales } from './built.js'
-import type { Locale, Dictionary } from './types'
+import { dictionaries } from './built.js'
+import type { Dictionary } from './types'
 
 const dict = derived(locale, ($locale) => dictionaries[$locale])
 
-export { dict, dictionaries, locales, locale }
-export type { Locale, Dictionary }
+export { dict, dictionaries, locale }
+export type { Dictionary }
