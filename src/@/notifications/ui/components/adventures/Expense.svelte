35c1e8e7638979aceea6f4075401673aae92570d
0xBADC0FFEE <a.burtsev@seed.me>
2026-03-13 18:58:23 +0300

    refactor(notifications): remove unnecessary adventure payer account fetch

--- src/@/notifications/ui/components/adventures/Expense.svelte
+++ src/@/notifications/ui/components/adventures/Expense.svelte
@@ -1,7 +1,5 @@
 <script lang="ts">
-  import { Async } from 'svas'
   import { currency } from '$lib/tools'
-  import { accounts } from '@/accounts'
   import { dict, locale } from '@/notifications/ui/intl'
   import Base from '../Base.svelte'
   import type { Props } from './Expense'
@@ -12,15 +10,11 @@
   const amount = $derived(currency(expense.amount, $locale))
 </script>
 
-<Async store={accounts.get(expense.payer)}>
-  {#snippet awaited(payer)}
-    <Base href={`/adventures/${notification.key}/expenses/${expense.id}/`}>
-      <div>
-        {$dict.adventures.expense.title(expense.title, amount)}
-        <p class="text-muted-foreground">
-          {$dict.adventures.expense.body(notification.payload.title)}
-        </p>
-      </div>
-    </Base>
-  {/snippet}
-</Async>
+<Base href={`/adventures/${notification.key}/expenses/${expense.id}/`}>
+  <div>
+    {$dict.adventures.expense.title(expense.title, amount)}
+    <p class="text-muted-foreground">
+      {$dict.adventures.expense.body(notification.payload.title)}
+    </p>
+  </div>
+</Base>
