3cb63d1b7be3701739bfc73b238976762b816ef1
temich <tema.gurtovoy@gmail.com>
2026-03-18 07:25:14 +0400

    feat(service): skip waiting on `dev`

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -40,20 +40,26 @@ app.addEventListener('install', (event) => {
 
     console.info(`${ASSETS.length} assets cached`)
     console.info(`App version ${version} installed in ${Date.now() - start}ms`)
+
+    if (dev)
+      app.skipWaiting()
   }
 
   event.waitUntil(install())
 })
 
 app.addEventListener('activate', (event) => {
-  async function deleteOldCaches() {
+  async function activate() {
     for (const key of await caches.keys())
       if (key !== CACHE) await caches.delete(key)
+
+    if (dev)
+      await app.clients.claim()
+
+    console.info('App activated')
   }
 
-  event.waitUntil(deleteOldCaches())
-
-  console.info('App activated')
+  event.waitUntil(activate())
 })
 
 app.addEventListener('fetch', (event) => {
