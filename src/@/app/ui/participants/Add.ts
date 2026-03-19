74f2b2bca0c6d2567af1b18ee660ea932d79ddf2
0xBADC0FFEE <a.burtsev@seed.me>
2026-03-19 17:53:44 +0300

    refactor(participants): omit onclick and href props in Add button

--- src/@/app/ui/participants/Add.ts
+++ src/@/app/ui/participants/Add.ts
@@ -1,7 +1,7 @@
 import type { ButtonProps } from '$ui/button'
 import type { ParticipantsOptions } from './Selector'
 
-export interface Props extends ButtonProps {
+export interface Props extends Omit<ButtonProps, 'onclick' | 'href'> {
   exclude: string[]
   onadd: (ids: string[]) => void
   options?: ParticipantsOptions
