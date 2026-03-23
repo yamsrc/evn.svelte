e7e9c1393af08026897645b9529eb8365f932f60
temich <tema.gurtovoy@gmail.com>
2026-03-23 15:42:35 +0400

    refactor(adventures): update card paddings

--- src/lib/components/text-ellipsis/TextEllipsis.ts
+++ src/lib/components/text-ellipsis/TextEllipsis.ts
@@ -1,9 +1,9 @@
 import type { Snippet } from 'svelte'
-import type { HTMLAttributes } from 'svelte/elements'
+import type { ClassValue, HTMLAttributes } from 'svelte/elements'
 
 export type Props = HTMLAttributes<HTMLSpanElement> & {
   children?: Snippet<[{ props: Record<string, unknown> }]>
-  class?: string
+  class?: ClassValue
 }
 
 export function textEllipsis() {
