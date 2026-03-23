520d75467c87a71761cb5b6a07220f8b448d2484
temich <tema.gurtovoy@gmail.com>
2026-03-23 17:47:53 +0400

    refactor(history): replace `__replace` with Symbol

--- src/lib/components/history/navigation.ts
+++ src/lib/components/history/navigation.ts
@@ -1,37 +1,30 @@
-import { writable, get } from 'svelte/store'
 import { goto } from '$app/navigation'
 import type { AfterNavigate } from '@sveltejs/kit'
 
-const history = writable<string[]>([])
+const history: string[] = []
 
 export function track(nav: AfterNavigate, state?: App.PageState) {
-  history.update((stack) => {
-    if (nav.to === null) return stack
+  if (nav.to === null) return history
 
-    const url = path(nav.to.url)
+  const url = path(nav.to.url)
+  const delta = nav.delta ?? 1
 
-    const delta = nav.delta ?? 1
-
-    if (delta < 0)
-      stack.splice(delta)
-    else if (state?.__replace)
-      stack[stack.length - 1] = url
-    else
-      stack.push(url)
-
-    return stack
-  })
+  if (delta < 0)
+    history.splice(delta)
+  else if (state !== undefined && replaced(state))
+    history[history.length - 1] = url
+  else
+    history.push(url)
 }
 
 /**
  * Find the closest match in the stack to the target from end
+ *
  * @param target - The target URL
  * @returns The closest match index in the stack to the target from end or -1 if not found
  */
 function closest(target: string): number {
-  const stack = [...get(history)].reverse()
-
-  return stack.findIndex((url) => url === target)
+  return history.toReversed().findIndex((url) => url === target)
 }
 
 function path(url: URL): string {
@@ -47,5 +40,20 @@ export async function back(href: string) {
 }
 
 export async function replace(href: string, state?: App.PageState) {
-  await goto(href, { replaceState: true, state: { ...state, __replace: true } })
+  const patched = patch(state)
+
+  await goto(href, { replaceState: true, state: patched })
 }
+
+function patch(state?: App.PageState): App.PageState {
+  return {
+    ...state,
+    [REPLACE]: true,
+  } as unknown as App.PageState
+}
+
+function replaced(state: App.PageState): boolean {
+  return (state as any)[REPLACE] === true
+}
+
+const REPLACE = Symbol('replace')
