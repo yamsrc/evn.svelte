f9897ae286dc2f88f08c5c34eda7196397014a78
temich <tema.gurtovoy@gmail.com>
2026-03-26 19:35:37 +0400

    fix(expenses): show expense title on participants page

--- src/routes/(private)/expenses/editor/[[id=id]]/add/+page.svelte
+++ src/routes/(private)/expenses/editor/[[id=id]]/add/+page.svelte
@@ -1,8 +1,11 @@
 <script lang="ts">
+  import { Editor } from '@/expenses/ui'
   import { Participants } from '@/app/ui'
   import { dict } from '$lib/intl'
+
+  const ctx = Editor.getContext()
 </script>
 
 <Participants.Selector
-  title={$dict.expenses.participants.title}
+  title={ctx.value.title || $dict.expenses.participants.title}
   options={{ managedContactsCreation: true }} />
