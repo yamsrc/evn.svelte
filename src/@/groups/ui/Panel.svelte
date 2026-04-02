ef09e99fbd6dd4966deeeac6e735efe6166f8c42
0xBADC0FFEE <a.burtsev@seed.me>
2026-04-02 19:28:30 +0300

    refactor(groups): show me in group avatars

--- src/@/groups/ui/Panel.svelte
+++ src/@/groups/ui/Panel.svelte
@@ -3,12 +3,9 @@
   import { Panel } from '$com/panel'
   import { Attention } from '$com/shell'
   import { Avatars, Balance } from '@/app/ui'
-  import { account } from '@/iam'
   import type { Props } from './Panel'
 
   let { group, selected = $bindable(), highlighted, onselect }: Props = $props()
-
-  const members = $derived(group.identities.filter((identity) => identity !== $account?.id))
 
   function onclick(event: MouseEvent) {
     if (onselect) {
@@ -34,7 +31,7 @@
         {/if}
         <span class="font-bold">{group.title ?? group.name}</span>
       </div>
-      <Avatars identities={members} max={5} class="py-1" />
+      <Avatars identities={group.identities} max={5} class="py-1" />
     </div>
   {/snippet}
   {#snippet right()}
