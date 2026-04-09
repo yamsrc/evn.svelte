8105b39e573dcee004d130aa67f0f804de80f50e
0xBADC0FFEE <a.burtsev@seed.me>
2026-04-09 18:26:35 +0300

    refactor(wallpaper): update presets

--- src/@/app/svc/wallpaper/pictures.ts
+++ src/@/app/svc/wallpaper/pictures.ts
@@ -1,12 +1,11 @@
 export const presets: string[] = [
-  'eb09bba5850ecbe81c6307bdb41c5980', // abstract
-  '21fde18dd102d8f513b7ad942bbfd97c', // baloons
-  '07997dd2085618191b3effb4b847d51d', // cafe
-  '4af8ded2721185b463ec1db936138649', // clouds
-  'caf0f2bbcdab84e0727c66adb69d986f', // curve
-  '68d2cab58de093e9b315e644351d5397', // dawn
-  '20821c610caa2e3a085f8829dbe9a06c', // eluminator
-  '18a16782a46594a765ef8e6a2613756a', // restaurant
-  '02af8a4690a3cdca521ef6bc15630e87', // sea
-  '2def2ffcdf3533d1c3560c8cfab6da6b', // street
+  '23c1ce02de858ad0dcf14aa6247a8f5c', // street
+  '1da6a51b42cda4a82a80403d1d4dd3cf', // cafe
+  '685081984a9357fc28b4f157d3acc571', // dawn
+  '9125b4f9713ace62736ba8b37fd138b6', // sea
+  '6db0aceb738e189d8f7c79a200da7377', // cluds
+  'a44071d87bd5869286a7dfac1d6a2059', // sun
+  '07c30dd9bee66239bb3e281fc6c5891e', // lava
+  '38eb08ae0b969279347c6baa7368d2c4', // curve
+  '90deaecdc7b2d2b21520c022ded2c9a1', // baloons
 ]
