5f001614dd963c28ec3eb98ee7b265437bb2dfd8
0xBADC0FFEE <a.burtsev@seed.me>
2026-04-17 16:19:14 +0300

    feat(graph): add premium ui

--- src/@/contacts/ui/graph/Graph.ts
+++ src/@/contacts/ui/graph/Graph.ts
@@ -5,7 +5,7 @@ import type { Account } from '@/accounts'
 import type { GridLayout } from './layout'
 import type { EdgeView, NodeView } from './Canvas'
 
-export type AccountLike = Pick<Account, 'id' | 'name' | 'picture'>
+export type AccountLike = Pick<Account, 'id' | 'name' | 'picture' | 'premium'>
 export type ContactLike = Pick<Contact, 'identities' | 'balance'>
 
 export interface Props {
