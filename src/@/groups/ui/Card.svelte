aea0f76d3ae4d1786c404052377687a02cb5e602
temich <tema.gurtovoy@gmail.com>
2026-05-11 06:07:50 +0400

    feat(groups): add card bg

--- src/@/groups/ui/Card.svelte
+++ src/@/groups/ui/Card.svelte
@@ -12,7 +12,7 @@
     'group-cover relative isolate overflow-hidden rounded-lg bg-cover bg-center py-3 px-4'
 </script>
 
-<div class={[base, 'size-full text-foreground border', classes]}>
+<div class={[base, 'size-full text-foreground bg-background border', classes]}>
   {#if group.picture}
     <Picture
       id={group.picture}
