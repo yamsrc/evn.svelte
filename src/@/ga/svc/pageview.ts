69986b0343b5159f58a9f9602ba9ca7db15e79af
temich <tema.gurtovoy@gmail.com>
2026-06-02 18:38:15 +0400

    feat(ga): remove debugging code

--- src/@/ga/svc/pageview.ts
+++ src/@/ga/svc/pageview.ts
@@ -1,8 +1,6 @@
 export function pageview(url: URL): void {
-  if (window.gtag !== undefined)
-    window.gtag?.('event', 'page_view', {
-      page_path: url.pathname + url.search,
-      page_location: url.href,
-    })
-  else console.debug('ga:pageview', url.href)
+  window.gtag?.('event', 'page_view', {
+    page_path: url.pathname + url.search,
+    page_location: url.href,
+  })
 }
--- src/@/ga/svc/track.ts
+++ src/@/ga/svc/track.ts
@@ -3,7 +3,5 @@ import type { EventName, TrackArgs } from './events'
 export function track<E extends EventName>(...args: TrackArgs<E>): void {
   const [event, data] = args
 
-  if (window.gtag !== undefined)
-    window.gtag?.('event', event, data)
-  else console.debug('ga:track', event, data)
+  window.gtag?.('event', event, data)
 }
