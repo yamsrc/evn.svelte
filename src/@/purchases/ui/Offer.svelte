f0de9f980a4b010bbe87f9b01b63b5e26cbe6be1
temich <tema.gurtovoy@gmail.com>
2026-06-02 11:39:02 +0400

    feat(purchases): add spinner

--- src/@/purchases/ui/Offer.svelte
+++ src/@/purchases/ui/Offer.svelte
@@ -127,7 +127,9 @@
     <div class="space-y-2 flex flex-col justify-between">
       <div class="rounded-lg ring-3 ring-primary/20">
         <Button {onclick} size="lg" class="w-full relative" disabled={busy}>
-          {#if free}
+          {#if busy}
+            <Spinner />
+          {:else if free}
             {$dict.paywall.free.cta}
           {:else if selected?.trial}
             {$dict.paywall.offer.trial.cta}
