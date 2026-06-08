1297de31292e36a6b4c00edb9cb8ae7f300c4eea
temich <tema.gurtovoy@gmail.com>
2026-06-08 12:38:01 +0400

    refactor(config): remove redundant `browser` check

--- src/config/features.ts
+++ src/config/features.ts
@@ -1,9 +1,8 @@
 import { satisfies } from 'semver'
 import { ios, shell, shellVersion } from '$lib/tools'
-import { browser } from '$app/environment'
 
-export const apple = shellVersion !== null && ios && satisfies(shellVersion, '>=1.6')
+export const apple = shell && ios && shellVersion !== null && satisfies(shellVersion, '>=1.6')
 
-export const stripe = browser && !shell
+export const stripe = !shell
 
 export const purchase = apple || stripe
