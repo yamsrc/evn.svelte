329018231b94c0f86ece266ea7da057723df71c7
temich <tema.gurtovoy@gmail.com>
2026-06-11 22:22:29 +0400

    feat(hello): add `vary`

--- src/routes/(public)/hello/+page.server.ts
+++ src/routes/(public)/hello/+page.server.ts
@@ -5,6 +5,7 @@ import type { PageServerLoad } from './$types'
 export const load: PageServerLoad = ({ request, setHeaders }) => {
   setHeaders({
     'cache-control': 'public, max-age=3600',
+    vary: 'accept-language',
   })
 
   const dict = dictionaries[acceptable(request.headers.get('accept-language'))]
